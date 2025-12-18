e2ed741a6d3fd25c1ae88b4402c78331309a9427
Pratik-Talegaonkar <pratiktalegaonkar13@gmail.com>
2025-12-18 20:17:25 +0530

    Correct answer for question

--- Chemistry.java
+++ Chemistry.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-public class Chemistry{
+
+public class Chemistry {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while(t-- > 0) {
             int n = in.nextInt();
             int k = in.nextInt();
-            String s = in.nextLine();
+            String s = in.next(); 
+            
             int freq[] = new int[26];
             for(int i = 0 ; i < n ; i++) {
                 freq[s.charAt(i) - 'a'] += 1;
@@ -17,7 +19,8 @@ public class Chemistry{
                 if(i % 2 != 0)
                     oddCases++;
             }
-            if(k == oddCases || oddCases == k + 1) {
+            
+            if(k >= oddCases - 1) {
                 System.out.println("YES");
             }
             else {
