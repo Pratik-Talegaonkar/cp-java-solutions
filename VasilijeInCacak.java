d8e803be13af366eee23ab232846e3d65b7d2077
Pratik-Talegaonkar <pratiktalegaonkar13@gmail.com>
2025-12-29 19:30:52 +0530

    pata nahi kya dikkat hai in logo ko :(

--- VasilijeInCacak.java
+++ VasilijeInCacak.java
@@ -3,19 +3,17 @@ public class VasilijeInCacak {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        while(t-- > 0) {
+        while (t-- > 0) {
             long n = in.nextLong();
             long k = in.nextLong();
             long x = in.nextLong();
 
-            long minSum = k*(k+1)/2;
-            long maxSum = k*(2*n-k+1)/2;
-            System.out.println("Min : " + minSum + "\tMaxSum : " + maxSum);
+            long minSum = k * (k + 1) / 2;
+            long maxSum = k * (2 * n - k + 1) / 2;
 
-            if(minSum <= x && x <= maxSum) {
+            if (x >= minSum && x <= maxSum) {
                 System.out.println("YES");
-            }
-            else {
+            } else {
                 System.out.println("NO");
             }
         }
